55863cd5e240f30a68d1b255cf7142a0d823eebe
Wolle <wolfram.wingerath@baqend.com>
2020-11-11 18:53:46 +0100

    fixed & extended formatters

--- code/formatters.py
+++ code/formatters.py
@@ -151,7 +151,7 @@ formatters_dict = {
 
 # This is the mapping from spoken phrases to formatters
 formatters_words = {
-    "allcaps": formatters_dict["ALL_CAPS"],
+    "all caps": formatters_dict["ALL_CAPS"],
     "alldown": formatters_dict["ALL_LOWERCASE"],
     "camel": formatters_dict["PRIVATE_CAMEL_CASE"],
     "dotted": formatters_dict["DOT_SEPARATED"],
@@ -168,6 +168,7 @@ formatters_words = {
     "snake": formatters_dict["SNAKE_CASE"],
     # "speak": formatters_dict["NOOP"],
     "string": formatters_dict["SINGLE_QUOTED_STRING"],
+    "dub string": formatters_dict["DOUBLE_QUOTED_STRING"],
     "title": formatters_dict["CAPITALIZE_ALL_WORDS"],
     # disable a few formatters for now
     # "tree": formatters_dict["FIRST_THREE"],
