1d238340091005b19fa3018b5be9b97364b231c5
Wolfram Wingerath <wolfram@wingerath.org>
2022-10-31 09:38:09 +0100

    gaming: toggle functions for SpeedRunners

--- lang/gaming/gaming_speedrunners.py
+++ lang/gaming/gaming_speedrunners.py
@@ -1,4 +1,4 @@
-from talon import Module, Context
+from talon import Module, Context, actions
 
 mod = Module()
 mod.tag("gaming_SpeedRunners", desc="game: SpeedRunners")
@@ -7,7 +7,19 @@ ctx.matches = "tag: user.gaming_SpeedRunners"
 
 
 ctx.settings["user.my_user_file_set_face_action_mode"] = "gaming_SpeedRunners"
+ctx.settings["user.my_user_file_set_face_mode_use_modifier"] = False
 
 #ctx.settings["user.my_user_file_set_eye_tracking_mode"] = "cursor"
 
 
+
+
+@mod.action_class
+class Actions:
+    def MouthRight_on():
+        """reacts to the MouthRight facial action commencing"""
+        actions.key("left:up")
+        actions.key("right:down")
+
+    def MouthRight_off():
+        """reacts to the MouthRight facial action stopping"""
